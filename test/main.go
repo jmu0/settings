3bfa7cc67c137f47c10dadf8c7b6d0b0d8122855
Jos Muysers <jos@muysers.nl>
2019-11-07 19:39:05 +0100

    test

---
+++ test/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jmu0/settings"
+)
+
+type test struct {
+	Een string `json:"een"`
+}
+
+func main() {
+	v := test{
+		Een: "test",
+	}
+	settings.Load("testfile.conf", &v)
+	//json.Unmarshal([]byte("{\"een\":\"eerste\"}"), &v)
+	fmt.Println("struct:", v)
+	// var str string
+	// fmt.Println(settings.Load("testfile.conf", &str))
+	fmt.Println()
+	s := map[string]string{}
+	settings.Load("testfile.conf", &s)
+	fmt.Println("map:", s)
+}
