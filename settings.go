51dc5557743bf719438bb7df9c59c77f2c806cf4
Jos Muysers <jos@muysers.nl>
2019-11-07 17:10:32 +0100

    figuring out reflect

--- settings.go
+++ settings.go
@@ -2,23 +2,67 @@ package settings
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
 	"strings"
 )
 
-//Settings struct
-type Settings struct {
-	File     string
-	Settings map[string]string
-	Loaded   bool
+//Load settings from file
+func Load(file string, s interface{}) error {
+	var err error
+	if reflect.ValueOf(s).Type().Kind() != reflect.Ptr {
+		return errors.New("Target is not a pointer")
+	}
+	testReflect(s)
+
+	if file != "" {
+		_, err = os.Stat(file)
+		if !os.IsNotExist(err) {
+			switch filepath.Ext(file) {
+			case ".conf":
+				err = loadConf(file, s)
+				if err != nil {
+					return err
+				}
+			case ".json":
+				err = loadJSON(file, s)
+				if err != nil {
+					return err
+				}
+			case ".yml":
+				err = loadYaml(file, s)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	err = loadEnvironmentVariables(s)
+	if err != nil {
+		return err
+	}
+	err = loadCommandLineArgs(s)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-//Load settings from file
-func (s *Settings) Load() (map[string]string, error) {
-	settings := map[string]string{}
-	str, err := readFile(s.File)
+func loadJSON(file string, settings interface{}) error {
+	return nil
+}
+func loadYaml(file string, settings interface{}) error {
+	return nil
+}
+
+func loadConf(file string, settings interface{}) error {
+	set := map[string]string{}
+	str, err := readFile(file)
 	if err != nil {
-		return settings, errors.New("Can't read settings file: " + s.File)
+		return err
 	}
 	lines := strings.Split(str, "\n")
 	if len(lines) > 0 {
@@ -26,40 +70,65 @@ func (s *Settings) Load() (map[string]string, error) {
 			if len(line) > 0 && line[:1] != "#" {
 				fields := strings.Fields(line)
 				if len(fields) > 1 {
-					settings[fields[0]] = strings.Join(fields[1:], " ")
+					set[fields[0]] = strings.Join(fields[1:], " ")
 				}
 			}
 		}
 	}
-	if len(settings) == 0 {
-		return settings, errors.New("No settings found in: " + s.File)
+	if len(set) == 0 {
+		return errors.New("No settings found in: " + file)
 	}
-	s.Settings = settings
-	s.Loaded = true
-	return settings, nil
+	fmt.Println(set)
+	settings = &set
+	return nil
+}
+
+func loadEnvironmentVariables(s interface{}) error {
+	return nil
+}
+
+func loadCommandLineArgs(s interface{}) error {
+	return nil
+}
+
+func testReflect(s interface{}) error {
+	switch reflect.ValueOf(s).Elem().Type().Kind() {
+	case reflect.Struct:
+		fmt.Println("I am a struct")
+		str := reflect.ValueOf(s).Elem()
+		fld := str.FieldByName("Een")
+		if fld.IsValid() {
+			fld.SetString("Aangepast!!")
+		}
+	case reflect.Map:
+		fmt.Println("I am a Map")
+	default:
+		return errors.New("Invalid target")
+	}
+	return nil
 }
 
 //Get get a setting
-func (s *Settings) Get(setting string) (string, error) {
-	if s.Loaded == false {
-		_, err := s.Load()
-		if err != nil {
-			return "", err
-		}
-	}
-	ret, ok := s.Settings[setting]
-	if !ok {
-		return ret, errors.New("Setting " + setting + " doesn't exist")
-	}
-	return ret, nil
-	//return "", errors.New("Setting " + setting + " doesn't exist") //for backwards compatibility (..doesn't end with return statement)
-}
+// func (s *Settings) Get(setting string) (string, error) {
+// 	if s.Loaded == false {
+// 		err := s.Load()
+// 		if err != nil {
+// 			return "", err
+// 		}
+// 	}
+// 	ret, ok := s.Settings[setting]
+// 	if !ok {
+// 		return ret, errors.New("Setting " + setting + " doesn't exist")
+// 	}
+// 	return ret, nil
+// 	//return "", errors.New("Setting " + setting + " doesn't exist") //for backwards compatibility (..doesn't end with return statement)
+// }
 
 //Set a setting
-func (s *Settings) Set(key, value string) error {
-	s.Settings[key] = value
-	return nil
-}
+// func (s *Settings) Set(key, value string) error {
+// 	s.Settings[key] = value
+// 	return nil
+// }
 
 //readFile read file into string
 func readFile(path string) (string, error) {
@@ -71,14 +140,14 @@ func readFile(path string) (string, error) {
 }
 
 //GetSettings return settings
-func GetSettings(filename string) (Settings, error) {
-	s := Settings{
-		File:   filename,
-		Loaded: false,
-	}
-	_, err := s.Load()
-	if err != nil {
-		return s, err
-	}
-	return s, nil
-}
+// func GetSettings(filename string) (interface{}, error) {
+// 	s := Settings{
+// 		File:   filename,
+// 		Loaded: false,
+// 	}
+// 	err := s.Load()
+// 	if err != nil {
+// 		return s, err
+// 	}
+// 	return s, nil
+// }
